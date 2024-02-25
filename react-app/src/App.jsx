11d7798525d8b65ab9eb29b522d28b57d7343c17
Adam Terchin <adam.terchin@gmail.com>
2024-02-25 12:29:34 -0500

    sass, bootstrap integration+overrides, fonts, cleanup vite defaults
    - fontsource github
    - temporary: button display for testing, other style guide stuff for examples
    
    SASS
    npm install -D sass
    
    FONTSOURCE
    npm install @fontsource-variable/inter

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import siteLogo from '/logoipsum-331.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='mb-4'>
+        <img src={siteLogo} className='logo' alt='Site logo' />
+        <h1>Wordpress + React</h1>
+        <button className='btn btn-outline-primary'>button outline</button>
+        <button className='btn btn-primary'>button primary</button>
+        <button className='btn btn-link'>Button Link</button>
+        <button className='btn btn-outline-secondary'>button outline secondary</button>
+        <button className='btn btn-secondary'>button secondary</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis odit facere quae voluptates minima nihil dolore
+        error, quibusdam molestias voluptas? Amet hic, in aspernatur accusamus animi enim facilis voluptates
+        necessitatibus.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores animi soluta culpa aspernatur modi
+        laboriosam similique omnis fugit aperiam facilis dolorem, iusto a, vel, laborum corrupti esse? Ad, ipsum labore?{' '}
+      </p>
+      <p className='my-5'>
+        <a href='https://vitejs.dev' target='_blank'>
+          This is a link to the Vite docs.
+        </a>
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
