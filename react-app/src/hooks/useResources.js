35ba59bb814e80498e0935bebe9be5b4e18265e1
Adam Terchin <adam.terchin@gmail.com>
2024-02-27 09:32:46 -0500

    pagination clicks working, loading page n posts - no categories/filters added

--- react-app/src/hooks/useResources.js
+++ react-app/src/hooks/useResources.js
@@ -1,27 +1,30 @@
-import { useState, useEffect } from 'react';
-import wordpress from '../apis/wordpress';
+import { useState, useEffect } from "react";
+import wordpress from "../apis/wordpress";
 
-function useResources({ setLoading, currentPage, setCurrentPage }) {
+function useResources({ setLoading, currentPage }) {
   const [resources, setResources] = useState([]);
   const [totalPosts, setTotalPosts] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
-    getResources();
-  }, []);
+    // console.log('Main effect');
+    getResources(currentPage);
+  }, [currentPage]);
 
   const getResources = async () => {
     let params = {};
+
+    params.page = currentPage;
     setLoading(true);
 
-    const response = await wordpress.get('/resources', { params });
+    const response = await wordpress.get("/resources", { params });
     //console.log("axios response", response);
 
-    if (Object.keys(response.headers).indexOf('x-wp-total') >= 0) {
-      setTotalPosts(parseInt(response.headers['x-wp-total']));
+    if (Object.keys(response.headers).indexOf("x-wp-total") >= 0) {
+      setTotalPosts(parseInt(response.headers["x-wp-total"]));
     }
-    if (Object.keys(response.headers).indexOf('x-wp-totalpages') >= 0) {
-      setTotalPages(parseInt(response.headers['x-wp-totalpages']));
+    if (Object.keys(response.headers).indexOf("x-wp-totalpages") >= 0) {
+      setTotalPages(parseInt(response.headers["x-wp-totalpages"]));
     }
 
     setResources(response.data);
