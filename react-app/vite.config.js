90dfb6f7eb200461c9ef5c38163f566e835840bf
Adam Terchin <adam.terchin@gmail.com>
2024-03-03 13:56:15 -0500

    Font tests - futura wasnt resolving where it was before (directly copies over in the public dir now) - testing inconsolata to see if there's file differences

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+  publicDir: "public",
   plugins: [react()],
-})
+});
