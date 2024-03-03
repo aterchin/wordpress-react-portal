4897b220b4665cf051d60d192ffb00c8341b48d8
Adam Terchin <adam.terchin@gmail.com>
2024-03-03 10:51:29 -0500

    term list components visually output - buttons, button groups

--- react-app/src/Components/BrandList.jsx
+++ react-app/src/Components/BrandList.jsx
@@ -1,4 +1,53 @@
-function BrandList() {
-  return <div>BrandList</div>;
+import { useState, useEffect } from "react";
+import wordpress from "../apis/wordpress";
+import { ToggleButtonGroup, ToggleButton } from "react-bootstrap";
+
+function BrandList({ onBrandSelect }) {
+  const [brands, setBrands] = useState([]);
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let params = {};
+      // ids separated by comma. can include/exclude specific ones
+      //params.include = 666;
+      const { data } = await wordpress.get("/brands", { params });
+      setBrands(data);
+    })();
+  }, []);
+
+  const handleChange = (val) => {
+    setValue(val);
+    onBrandSelect(val);
+  };
+
+  const items = brands.map((item) => {
+    return (
+      <ToggleButton
+        key={item.id}
+        id={`brand-${item.id}`}
+        value={item.id}
+        className={`mb-2${brands.length === 1 ? " pe-none" : ""}`}
+        variant="outline-secondary"
+        size="lg"
+      >
+        {item.name}
+      </ToggleButton>
+    );
+  });
+
+  return (
+    <div className="brand-list-wrapper">
+      <ToggleButtonGroup
+        vertical="true"
+        type="checkbox"
+        value={value}
+        onChange={handleChange}
+        className="mb-4"
+      >
+        {items}
+      </ToggleButtonGroup>
+    </div>
+  );
 }
 export default BrandList;
--- react-app/src/Components/CategoryList.jsx
+++ react-app/src/Components/CategoryList.jsx
@@ -1,4 +1,49 @@
-function CategoryList() {
-  return <div>CategoryList</div>;
+import { useState, useEffect } from "react";
+import wordpress from "../apis/wordpress";
+import { ToggleButtonGroup, ToggleButton } from "react-bootstrap";
+
+function CategoryList({ onCategorySelect }) {
+  const [categories, setCategories] = useState([]);
+  const [value, setValue] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let params = {};
+      // ids separated by comma. can include/exclude specific ones
+      //params.include = 666;
+      const { data } = await wordpress.get("/categories", {
+        params,
+      });
+      setCategories(data);
+    })();
+  }, []);
+
+  const handleChange = (val) => {
+    setValue(val);
+    onCategorySelect(val);
+  };
+
+  const items = categories.map((item) => {
+    return (
+      <ToggleButton
+        key={item.id}
+        id={`category-${item.id}`}
+        value={item.id}
+        className="mb-2"
+        variant="outline-secondary"
+        size="sm"
+      >
+        {item.name}
+      </ToggleButton>
+    );
+  });
+
+  return (
+    <div className="category-list-wrapper mb-4">
+      <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
+        {items}
+      </ToggleButtonGroup>
+    </div>
+  );
 }
 export default CategoryList;
